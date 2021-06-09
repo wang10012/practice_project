e55ad967d9335e01b6af92f3fa1b39ea8382cc2f
sword <53691746+wang10012@users.noreply.github.com>
2021-06-10 01:57:24 +0800

    new

--- file_encrypt/main.py
+++ file_encrypt/main.py
@@ -44,9 +44,9 @@ class file(object):
         if self.encryption_install_point in self.path_parts and self.access == 'w':
             self.is_encrypted = True
             self.index = self.path_parts.index(self.encryption_install_point)
-            with open(self.file_now_path, 'r') as f:
+            with open(self.file_now_path, 'r', encoding='utf-8') as f:
                 cipher_text = base64.b64encode(cipher.encrypt(f.read().encode('utf-8')))
-            with open(self.file_now_path, 'w') as fd:
+            with open(self.file_now_path, 'w', encoding='utf-8') as fd:
                 fd.write(cipher_text.decode('utf-8'))
             print('此明文文件写入目标路径的时候经过加密安装点，已经被加密，其加密安装点位于文件路径的第%s层' % (self.index + 1))
         else:
@@ -58,13 +58,16 @@ class file(object):
         if self.encryption_install_point in self.path_parts and self.access == 'r':
             self.is_encrypted = False
             self.index = self.path_parts.index(self.encryption_install_point)
-            with open(self.file_now_path, 'r') as f:
-                plain_text = cipher.decrypt(base64.b64decode(f.read().encode("utf-8")),random_generator)
+            with open(self.file_now_path, 'r', encoding='utf-8') as f:
+                plain_text = cipher.decrypt(base64.b64decode(f.read().encode("utf-8")), random_generator)
                 print('此密文文件的路径经过加密安装点，已经被解密，其加密安装点位于文件路径的第%s层' % (self.index + 1))
                 print("解密后的密文为：")
                 print(plain_text.decode('utf-8'))
         else:
             print("此密文文件的路径未经过加密安装点")
+            with open(self.file_now_path, 'r', encoding='utf-8') as f:
+                print("所读文件的内容是：")
+                print(f.read())
 
 
 def encrypt(file_now_path, file_dst_path, access, encryption_install_point):
@@ -99,9 +102,16 @@ def mymovefile(file_now_path, file_dst_path, access, encryption_install_point):
 
 def main():
     while True:
+        print("************************************************")
+        print("文件加密模拟系统")
+        print("作者：王诗剑")
+        print("************************************************")
         dirtree = DirectionTree()
         print("请输入您选择的文件系统：")
         path = input()
+        if path == "quit":
+            print("结束程序！")
+            break
         if Path(path).exists():
             dirtree.set_path(path)
             dirtree.generate_tree()
@@ -110,18 +120,48 @@ def main():
             print("当前路径不存在")
         print("请输入您选择的加密安装点：")
         encryption_install_point = input()
-        print("请输入您要操作的文件的当前路径:")
-        file_now_path = input()
-        print("请输入您对文件的操作：")
+        if encryption_install_point not in dirtree.tree:
+            print("您输入的加密安装点不存在当前路径")
+            continue
+        print("请输入您将要对文件的操作：")
         access = input()
-        # 读
+        # 写
         if access == 'w':
-            print("请输入您文件要写入的文件夹地址：")
-            file_dst_path = input()
-            if mymovefile(file_now_path, file_dst_path, access, encryption_install_point):
-                print("已经读入成功！")
-        if access == 'r':
-            decrypt(file_now_path, '', access, encryption_install_point)
+            print("请问您要选择批量写入还是单个文件写入：(单个文件写入：输入1  批量文件写入：输入2)")
+            order = int(input())
+            if order == 1:
+                print("请输入您要操作的单个文件的当前路径:")
+                file_now_path = input()
+                print("请输入您文件要写入的文件夹地址：")
+                file_dst_path = input()
+                if mymovefile(file_now_path, file_dst_path, access, encryption_install_point):
+                    print("已经读入成功！")
+            elif order == 2:
+                print("请输入批量文件所在的文件夹路径：")
+                dir_path = input()
+                print("请输入这些批量文件要写入的文件夹地址：")
+                dst_path = input()
+
+                def recursive(dir_path):
+                    for item in os.listdir(dir_path):
+                        path = dir_path + "\\" + item
+                        if os.path.isfile(path):
+                            if mymovefile(path, dst_path, access, encryption_install_point):
+                                print("已经读入成功！")
+                        if os.path.isdir(path):
+                            recursive(path)
+
+                recursive(dir_path)
+            else:
+                print("您输入的操作有误！请重新开始")
+
+        elif access == 'r':
+            print("请输入您想读的文件路径：")
+            read_file_path = input()
+            decrypt(read_file_path, '', access, encryption_install_point)
+        else:
+            print("您输入的操作方式有误")
+            continue
 
 
 if __name__ == '__main__':
